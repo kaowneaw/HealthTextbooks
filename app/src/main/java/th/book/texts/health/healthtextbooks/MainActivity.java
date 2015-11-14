c4f29ec9f1878eb7ddcbbad5ad56706e08a5ee5b
Panya Noipram <panya.n@baanprogram.com>
2015-11-14 20:48:17 +0700

    initial commit

---
+++ app/src/main/java/th/book/texts/health/healthtextbooks/MainActivity.java
@@ -0,0 +1,13 @@
+package th.book.texts.health.healthtextbooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
